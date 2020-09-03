f37ed570e840bd6d6b545a20feaf70ffa7eeed1e
your name <your@email.com>
2020-09-03 10:58:38 +0100

    Initial Commit

---
+++ EsploraDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EsploraDemoApp
+//
+//  Created by Asaad on 9/3/20.
+//  Copyright © 2020 Animata Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
